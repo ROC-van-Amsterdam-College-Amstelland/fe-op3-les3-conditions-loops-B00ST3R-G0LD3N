5fba46eceeb97efe463fde9851eeed75a0f44050
Sofyan Ahalli <sofyan_a@hotmail.com>
2019-12-03 11:17:09 +0100

    saldo vragen

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,13 @@
 //javascript code
+var geld= 10; 
+ geld = prompt("Hoeveel geld staat er op uw rekening?")
+
+if (geld <25) {
+    alert("sorry u heeft te weinig saldo");
+
+} else{
+    alert("Aanvraag geaccepteerd");
+
+}
+
+
--- conditions_opdrachten/conditions_opdracht_2/js/script.js
+++ conditions_opdrachten/conditions_opdracht_2/js/script.js
@@ -1,1 +1,21 @@
 //javascript code
+var student= sofyan; 
+ student = prompt("Hoeveel geld staat er op uw rekening?")
+
+if (geld <25) {
+    alert("sorry u heeft te weinig saldo");
+
+} else{
+    alert("Aanvraag geaccepteerd");
+
+}
+
+var student = {                
+    firstName:"sofyan",        
+    lastName:"ahalli",
+    age:17,
+    height:180,
+    fullName : function() {
+       return this.firstName + " " + this.lastName;
+
+    }
