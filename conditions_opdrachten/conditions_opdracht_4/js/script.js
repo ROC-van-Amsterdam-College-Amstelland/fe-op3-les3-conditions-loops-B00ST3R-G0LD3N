b46e6709e860e6c3519adddcffe8ba00763d7265
Sofyan Ahalli <sofyan.ahalli@student.rocva.nl>
2019-12-05 11:39:19 +0100

    opdracht 4

--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,2 +1,6 @@
 //javascript code
-a
+var myNumber = 10;
+var text = "De variabele myNumber heeft de waarde" + myNumber;
+alert(text);
+
+// var myNumber = "De variabele myNumber heeft de waarde: 10", myNumber = "De variabele myNumber heeft de waarde: 10";
